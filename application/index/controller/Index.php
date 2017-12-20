bad5ad267f986443236545b472e3a0a1d4b781f2
hexiaojiao <laurel_he@163.com>
2017-12-20 10:31:49 +0800

    修改主页

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,26 @@
 <?php
 namespace app\index\controller;
-
-class Index
+use think\Controller;
+use think\Request;
+use think\Db;
+class Index extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        $user_session = $request->session('user_name');
+        if($user_session)
+        {
+            $login = "欢迎您".$user_session;
+        }
+        else
+        {
+            $login = "请先登陆";
+        }
+        $hot_rank = Db::name('blog')->order('praise desc')->limit('3')->select();
+        $time_rank = Db::name('blog')->order('blog_time desc')->limit('3')->select();
+        $this->assign('hot_rank',$hot_rank);
+        $this->assign('login',$login);
+        $this->assign('time_rank',$time_rank);
+        return $this->fetch();
     }
 }
